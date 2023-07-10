e1034c49abaf4fc4b00ab1179a6f8cbe1589d216
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2023-07-10 20:31:20 +0530

    Create 10_july.java

---
+++ 10_july.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if (root.left != null && root.right != null)
+            return Math.min(left, right) + 1;
+
+        return Math.max(left, right) + 1;
+    }
+}
