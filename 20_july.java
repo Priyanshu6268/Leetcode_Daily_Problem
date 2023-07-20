2dc44e6fef70b398db6572f70784744375e79829
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2023-07-20 14:17:18 +0530

    Create 20_july.java

---
+++ 20_july.java
@@ -0,0 +1,15 @@
+//.   https://leetcode.com/problems/asteroid-collision/description/
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+      LinkedList<Integer> list=new  LinkedList<>();
+      for (int i : asteroids) {
+            while (!list.isEmpty() && list.getLast() > 0 && list.getLast() < -i)
+                list.pollLast();
+            if (list.isEmpty() || i > 0 || list.getLast() < 0)
+                list.add(i);
+            else if (i < 0 && list.getLast() == -i)
+                list.pollLast();
+        }
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
