58bfd5d22d10352b069cf444cb6bfa4fe5d7b46c
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-02-21 11:03:59 +0530

    Create 21Feb24.java

---
+++ 21Feb24.java
@@ -0,0 +1,12 @@
+//   https://leetcode.com/problems/bitwise-and-of-numbers-range/
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+        return (left << cnt);
+    }
+}
