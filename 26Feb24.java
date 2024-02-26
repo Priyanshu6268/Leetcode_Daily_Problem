5bb113caee0ed9bd26ef3709a31c00029dfa978f
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-02-26 15:03:53 +0530

    Create 26Feb24.java

---
+++ 26Feb24.java
@@ -0,0 +1,8 @@
+//.   https://leetcode.com/problems/same-tree/description/
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
