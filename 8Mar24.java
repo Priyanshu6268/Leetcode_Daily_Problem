884b6ad32d6af94a11d451f2cb04dc2df4e93f88
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-03-08 15:02:53 +0530

    Create 8Mar24.java

---
+++ 8Mar24.java
@@ -0,0 +1,21 @@
+//        https://leetcode.com/problems/count-elements-with-maximum-frequency/
+class Solution 
+{
+    public int maxFrequencyElements(int[] nums) 
+    {
+        int kMax = 100;
+        int ans = 0;
+        int[] count = new int[kMax + 1];
+
+        for (int num : nums)
+            ++count[num];
+
+        int maxFreq = Arrays.stream(count).max().getAsInt();
+
+        for (int freq : count)
+            if (freq == maxFreq)
+                ans += maxFreq;
+
+        return ans;
+    }
+}
