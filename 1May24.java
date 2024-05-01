91a8e99ec98b78a67cf96fa309856572b5cc26a0
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-05-01 10:36:04 +0530

    Create 1May24.java

---
+++ 1May24.java
@@ -0,0 +1,10 @@
+//      https://leetcode.com/problems/reverse-prefix-of-word/
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int j = word.indexOf(ch);
+        if (j != -1) {
+            return new StringBuilder(word.substring(0, j + 1)).reverse().toString() + word.substring(j + 1);
+        }
+        return word;
+    }
+}
