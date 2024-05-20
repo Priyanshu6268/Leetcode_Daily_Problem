809ad8fa173c1e860634cd1b1a83022250fa0387
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-05-20 11:50:05 +0530

    Create 20May24.java

---
+++ 20May24.java
@@ -0,0 +1,18 @@
+//      https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        // First create helper function, this function will help us to calculate
+        // when i'm including, when i'm not. The XOR of that array
+        return helper(nums, 0, 0); // nums, level, current XOR
+    }
+    public int helper(int nums[], int level, int currentXOR){
+        // base condition
+        if(level == nums.length) return currentXOR;
+        // creating include 
+        int include = helper(nums, level + 1, currentXOR^nums[level]);
+        // creating exclude
+        int exclude = helper(nums, level + 1, currentXOR);
+        
+        return include + exclude;
+    }
+}
