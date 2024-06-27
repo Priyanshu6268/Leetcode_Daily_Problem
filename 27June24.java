4380ae1befcfbd81d220adcc422a52eb4b718e7a
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-06-27 14:30:37 +0530

    Create 27June24.java

---
+++ 27June24.java
@@ -0,0 +1,20 @@
+//      https://leetcode.com/problems/find-center-of-star-graph/description/
+class Solution {
+    public int findCenter(int[][] edges) {
+        int n = edges.length + 1;
+        int[] degreeCount = new int[n + 1];
+
+        for (int[] edge : edges) {
+            degreeCount[edge[0]]++;
+            degreeCount[edge[1]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (degreeCount[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
