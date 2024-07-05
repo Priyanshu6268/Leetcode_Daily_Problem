e46b14f1382b3fdb9eaeb28b8aec4d23fabb8ff3
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-07-05 10:51:18 +0530

    Create 5July24.cpp

---
+++ 5July24.cpp
@@ -0,0 +1,28 @@
+//    https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/description/
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) 
+    {
+        vector<int>store;
+
+        int ans1 = INT_MAX, cur, next, n;
+        int last = head->val, position = 2;
+
+        while(head->next)
+        {
+            cur = head->val, next = head->next->val;
+
+            if((last > cur and cur < next) or (last < cur and cur > next)) 
+                store.push_back(position);
+            position++;
+            last = cur, head = head->next;
+
+            n = store.size();
+            if(n > 1) ans1 = min(ans1, store[n-1] - store[n-2]);
+        }
+        
+        if(ans1 == INT_MAX) return {-1, -1};
+        
+        return {ans1, store.back() - store[0]};
+    }
+};
