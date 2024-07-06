2a1df7da8e9f778709805c6cf20bfb4ff3a7ceb4
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-07-06 09:21:10 +0530

    Create 6July24.cpp

---
+++ 6July24.cpp
@@ -0,0 +1,9 @@
+//    https://leetcode.com/problems/pass-the-pillow/description/
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int div = time / (n - 1);
+        int rem = time % (n - 1);
+        return div % 2 == 0 ? rem + 1 : n - rem;
+    }
+};
