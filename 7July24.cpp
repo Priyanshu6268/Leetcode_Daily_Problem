856443c84fc1d9730a3b0ce7738bdbea160addfe
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-07-07 20:01:28 +0530

    Create 7July24.cpp

---
+++ 7July24.cpp
@@ -0,0 +1,14 @@
+//      https://leetcode.com/problems/water-bottles/description/
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int totalBottles = numBottles;
+
+        while (numBottles >= numExchange) {
+            totalBottles += numBottles / numExchange;
+            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
+        }
+
+        return totalBottles;
+    }
+};
