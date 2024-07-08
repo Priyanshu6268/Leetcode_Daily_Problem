ebb3f15b84d1fc58b72a6fb1d05d940b8d3f002c
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-07-08 09:31:48 +0530

    Create 8July24.cpp

---
+++ 8July24.cpp
@@ -0,0 +1,11 @@
+//    https://leetcode.com/problems/find-the-winner-of-the-circular-game/description/
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int winner=0;
+        for (int i = 1; i <= n; i++) {
+            winner = (winner + k) % i;
+        }
+        return winner + 1;
+    }
+};
