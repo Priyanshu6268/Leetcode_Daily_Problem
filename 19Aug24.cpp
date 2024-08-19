6c64c8842158d3c86bef2f72db4a526eb86518cd
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-08-19 18:53:14 +0530

    Create 19Aug24.cpp

---
+++ 19Aug24.cpp
@@ -0,0 +1,14 @@
+//    https://leetcode.com/problems/2-keys-keyboard/description/
+class Solution {
+public:
+    int minSteps(int n) {
+        int ops = 0;
+        for(int fac = 2; fac <= n; fac++){
+            while(n % fac == 0){
+                ops+=fac;
+                n /= fac;
+            }
+        }
+        return ops;
+    }
+};
