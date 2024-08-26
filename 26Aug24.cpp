7cf23d9a47a6aa784465e7241b69200fdf3cd0b9
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-08-26 20:55:11 +0530

    Create 26Aug24.cpp

---
+++ 26Aug24.cpp
@@ -0,0 +1,17 @@
+//    https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+class Solution {
+public:
+    vector<int>a;
+    void postOrder(Node* root)
+    {
+        if(root == NULL) return;
+        for(Node* child: root->children)
+            postOrder(child);
+        a.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) 
+    {
+        postOrder(root);
+        return a;
+    }
+};
