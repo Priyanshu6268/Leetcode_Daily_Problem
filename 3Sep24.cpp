791e91b0c6f78c25c74d2690d7b40c7b2d53e378
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-09-03 16:54:20 +0530

    Create 3Sep24.cpp

---
+++ 3Sep24.cpp
@@ -0,0 +1,27 @@
+//    https://leetcode.com/problems/sum-of-digits-of-string-after-convert/description/
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string sb;
+        int sum = 0;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            sb += to_string(((s[i]) - 'a' + 1));
+        }
+        for(int i=0;i<sb.length();i++){
+            sum += ((sb[i])-'0');
+        }
+        k--;
+        while(k!=0){
+            int num = sum;
+            sum = 0;
+            while(num!=0){
+                int a = num%10;
+                num/=10;
+                sum += a;
+            }
+            k--;
+        }
+        return sum;
+    }
+};
