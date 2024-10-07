9c13653dea66e4bc291149cd7a246177c6c7f16d
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-10-07 07:31:44 +0530

    Create 7Oct24.cpp

---
+++ 7Oct24.cpp
@@ -0,0 +1,26 @@
+//      https://leetcode.com/problems/minimum-string-length-after-removing-substrings/description/
+class Solution {
+public:
+    int minLength(string s) 
+    {
+        stack<char>st;
+        for(auto ch:s)
+        {
+            if(!st.empty())
+            {
+                if(st.top() == 'A' and ch == 'B')
+                {
+                    st.pop();
+                    continue;
+                }
+                if(st.top() == 'C' and ch == 'D')
+                {
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(ch);
+        }
+        return st.size();
+    }
+};
