0de99ac7d9b228a6c2937e19cd11ec29a64a73f0
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-10-14 06:51:58 +0530

    Create 14Oct24.cpp

---
+++ 14Oct24.cpp
@@ -0,0 +1,19 @@
+//    https://leetcode.com/problems/maximal-score-after-applying-k-operations/description/
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) 
+    {
+        priority_queue<int>pq;
+        for(auto val:nums) 
+            pq.push(val);
+        
+        long long ans = 0, val;
+        while(k--)
+        {
+            val = pq.top(); pq.pop();
+            ans += val;
+            pq.push(ceil(double(val/3.0)));
+        }
+        return ans;
+    }
+};
