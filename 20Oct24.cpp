d107c976d0d21fb0f569974f405f73b016c18437
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-10-20 07:55:47 +0530

    Create 20Oct24.cpp

---
+++ 20Oct24.cpp
@@ -0,0 +1,41 @@
+//    https://leetcode.com/problems/parsing-a-boolean-expression/description/
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+    stack<char> stk;
+
+    for (char ch : expression) {
+        if (ch == ',' || ch == '(') {
+            continue;
+        }
+
+        if (ch == 't' || ch == 'f' || ch == '!' || ch == '&' || ch == '|') {
+            stk.push(ch);
+        } else if (ch == ')') {
+            bool hasTrue = false, hasFalse = false;
+            while (stk.top() == 't' || stk.top() == 'f') {
+                hasTrue |= stk.top() == 't';
+                hasFalse |= stk.top() == 'f';
+                stk.pop();
+            }
+
+            char op = stk.top();
+            stk.pop();
+
+            bool result;
+            if (op == '!') {
+                result = !hasTrue;
+            } else if (op == '&') {
+                result = !hasFalse;
+            } else {
+                result = hasTrue;
+            }
+
+            stk.push(result ? 't' : 'f');
+        }
+    }
+
+    return stk.top() == 't';
+}
+
+};
