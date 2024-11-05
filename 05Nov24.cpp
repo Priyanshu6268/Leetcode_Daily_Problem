ac081eebc12b29b85cf122208e99dad6228b0f01
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-11-05 12:49:02 +0530

    Create 05Nov24.cpp

---
+++ 05Nov24.cpp
@@ -0,0 +1,13 @@
+//    https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful/description/
+class Solution {
+public:
+    int minChanges(const std::string& s) {
+    int count = 0;
+    for (int i = 0; i < s.size() - 1; i += 2) {
+        if (s[i] != s[i + 1]) {
+            count++;
+        }
+    }
+    return count;
+}
+};
