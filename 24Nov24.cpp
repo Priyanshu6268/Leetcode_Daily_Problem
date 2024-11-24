f36531bbbf7fbc54f8fa83ae0049104d958d8b03
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-11-24 09:10:33 +0530

    Create 24Nov24.cpp

---
+++ 24Nov24.cpp
@@ -0,0 +1,21 @@
+//    https://leetcode.com/problems/maximum-matrix-sum/description/
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long sum=0;
+        int minAbs=INT_MAX, cntNeg=0;
+        for(auto& row: matrix){
+            for(int x: row){
+                minAbs=min(minAbs, abs(x));
+                if (x<0){
+                    sum-=x;
+                    cntNeg++;
+                }
+                else{
+                    sum+=x;
+                }
+            }
+        }
+        return (cntNeg&1)?sum-2*minAbs:sum;
+    }
+};
