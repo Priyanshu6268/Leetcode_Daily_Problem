f8d45eade8ee00ede8e7358209fa9f1ed88b07c9
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-12-01 10:26:42 +0530

    Create 01Dec24.cpp

---
+++ 01Dec24.cpp
@@ -0,0 +1,19 @@
+//    https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) 
+    {
+        map<int,int>mp;
+        mp[0] = -1;
+        for(auto val:arr) mp[val]++;
+        for(auto val:arr)
+        {
+            if(val%2) 
+            {
+                if(mp[val+val]) return true;
+            }
+            else if(mp[val/2]) return true;
+        }
+        return false;
+    }
+};
