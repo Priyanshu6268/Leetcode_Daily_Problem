fd7751cfc4cab5f716e9c8332d9a97b608b211b5
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-12-03 07:05:33 +0530

    Create 03Dec24.cpp

---
+++ 03Dec24.cpp
@@ -0,0 +1,18 @@
+//    https://leetcode.com/problems/adding-spaces-to-a-string/description/
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) 
+    {
+        string ans;
+        int i = 0, j = 0;
+        int n = s.size(), m = spaces.size();
+        while(j < m)
+        {
+            if(i == spaces[j])
+                ans += ' ', j++;
+            ans += s[i++];
+        }
+        while(i < n) ans += s[i++];
+        return ans;
+    }
+};
