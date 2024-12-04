107dbb403602bee4e0d08bb391edcbbf6a49c30b
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-12-04 21:41:46 +0530

    Create 04Dec24.cpp

---
+++ 04Dec24.cpp
@@ -0,0 +1,21 @@
+//    https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments/description/
+class Solution {
+public:
+    static bool canMakeSubsequence(string& source, string& target) {
+        const int srcLen = source.size(), tgtLen = target.size();
+        char targetChar = target[0];
+        
+        int srcIdx, tgtIdx;
+        for(srcIdx = 0, tgtIdx = 0; srcIdx < srcLen && tgtIdx < tgtLen; srcIdx++) {
+            char srcChar = source[srcIdx];
+            
+            if (srcChar == targetChar || 
+                srcChar + 1 == targetChar || 
+                (srcChar == 'z' && targetChar == 'a')) {
+                targetChar = target[++tgtIdx];
+            }
+        }
+        
+        return tgtIdx == tgtLen;
+    }
+};
