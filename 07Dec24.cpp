86f5ff6ee02be6b8a8c80da47f257e655f17a59f
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-12-07 07:36:59 +0530

    Create 07Dec24.cpp

---
+++ 07Dec24.cpp
@@ -0,0 +1,18 @@
+//      https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/description/
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int k) {
+        const int n=nums.size();
+    //    sort(nums.begin(), nums.end());
+        int l=1, r=*max_element(nums.begin(), nums.end()), m;
+        while(l<r){
+            m=l+(r-l)/2;
+            long long cnt=0;
+            for(int j=0; j<n && cnt<=k; j++)
+                cnt+=(nums[j]-1)/m;
+            if(cnt<=k) r=m;
+            else l=m+1;
+        }
+        return r;
+    }
+};
