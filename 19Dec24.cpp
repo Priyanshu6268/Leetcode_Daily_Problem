44b47c05758bbdc3f6601ec766b3ab9341387170
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-12-19 09:34:15 +0530

    Create 19Dec24.cpp

---
+++ 19Dec24.cpp
@@ -0,0 +1,12 @@
+//      https://leetcode.com/problems/max-chunks-to-make-sorted/description/
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int ans = 0, maxi = INT_MIN;
+        for(int i = 0; i<arr.size(); i++){
+            maxi = max(maxi, arr[i]);
+            if(maxi == i) ans++;
+        }
+        return ans;
+    }
+};
