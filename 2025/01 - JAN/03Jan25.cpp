decc8038a21be26a25bcf6e5ce2994bf2d24e5e0
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-01-03 09:19:01 +0530

    Create 03Jan25.cpp

---
+++ 2025/01 - JAN/03Jan25.cpp
@@ -0,0 +1,23 @@
+//    https://leetcode.com/problems/number-of-ways-to-split-array/description/
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        long long leftSideSum = 0, rightSideSum = 0;
+        for (int num : nums) {
+            rightSideSum += num;
+        }
+
+        int validSplits = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            leftSideSum += nums[i];
+            rightSideSum -= nums[i];
+
+            if (leftSideSum >= rightSideSum) {
+                validSplits++;
+            }
+        }
+
+        return validSplits;
+    }
+};
