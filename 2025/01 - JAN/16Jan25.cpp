12e66f93f660c1fff252d8f1998d5573c94072fa
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-01-16 09:36:20 +0530

    Create 16Jan25.cpp

---
+++ 2025/01 - JAN/16Jan25.cpp
@@ -0,0 +1,7 @@
+//    https://leetcode.com/problems/bitwise-xor-of-all-pairings/description/
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        return ((nums2.size()&1)?accumulate(nums1.begin(), nums1.end(), 0, bit_xor<>{}):0)^((nums1.size()&1)?accumulate(nums2.begin(), nums2.end(), 0, bit_xor<>{}):0);
+    }
+};
