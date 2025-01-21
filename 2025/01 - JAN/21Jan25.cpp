0b4e6cd117976b28657a45f0c8b979a0205daaa3
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-01-21 09:17:16 +0530

    Create 21Jan25.cpp

---
+++ 2025/01 - JAN/21Jan25.cpp
@@ -0,0 +1,20 @@
+//        https://leetcode.com/problems/grid-game/description/
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        long long ans = 1e18, suf = 0, pre = 0;
+
+        for(auto i : grid[0])suf += i;
+
+        for(int i = 0;i < grid[0].size() ;i++){
+            suf -= grid[0][i];
+
+            if(i)
+                pre += grid[1][i - 1];
+
+            ans = min(ans, max(pre, suf));
+        }
+
+        return ans;
+    }
+};
