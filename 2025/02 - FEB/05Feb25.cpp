5f1ce38a2ea070eaf990ecc03026e63485e8e899
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-02-05 10:08:42 +0530

    Create 05Feb25.cpp

---
+++ 2025/02 - FEB/05Feb25.cpp
@@ -0,0 +1,9 @@
+//    https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/description/
+class Solution {
+public:
+    int cnt = 0; char a, b;
+    bool areAlmostEqual(string s, string t) {
+        for (int i = 0; i < s.size(); i++) cnt = s[i] != t[i] ? cnt == 0 ? (a = s[i], b = t[i], cnt + 1) : cnt == 1 ? (s[i] == b && t[i] == a ? cnt + 1 : 3) : 3 : cnt;
+        return cnt == 0 || cnt == 2;
+    }
+};
