b926dcc29c4fe528a0dbbdf0247ecf2174d39e47
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-02-12 10:39:34 +0530

    Create 12Feb25.cpp

---
+++ 2025/02 - FEB/12Feb25.cpp
@@ -0,0 +1,28 @@
+//    https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/description/
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        unordered_map<int, vector<int>> map;
+        for (int num : nums) {
+            int sum = digSum(num);
+            map[sum].push_back(num);
+        }
+        int maxSum = -1;
+        for (auto& [key, arr] : map) {
+            if (arr.size() >= 2) {
+                nth_element(arr.begin(), arr.end() - 1, arr.end());
+                nth_element(arr.begin(), arr.end() - 2, arr.end());
+                maxSum = max(maxSum, arr[arr.size() - 1] + arr[arr.size() - 2]);
+            }
+        }
+        return maxSum;
+    }
+    int digSum(int x) {
+        int sum = 0;
+        while (x != 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    }
+};
