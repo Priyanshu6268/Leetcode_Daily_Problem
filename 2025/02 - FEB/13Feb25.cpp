046fa5035eba953010f7a8acac84bd8f8ea6fe6a
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-02-13 10:20:40 +0530

    Create 13Feb25.cpp

---
+++ 2025/02 - FEB/13Feb25.cpp
@@ -0,0 +1,22 @@
+//        https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-ii/description/
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        priority_queue<long long int, vector<long long int>, greater<long long int>> pq;
+        for(auto it: nums){
+            pq.push(it);
+        }
+        long long int cnt=0;
+        while(pq.top()<k){
+            long long int first = pq.top();
+            pq.pop();
+            long long int second = pq.top();
+            pq.pop();
+            long long int num = 2*min(first, second) + max(first, second);
+            pq.push(num);
+            cnt++;
+        }
+        return cnt;
+    }
+};
