3f535d479d9de0de2fc3c42f33e2673f4eabcf33
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-02-18 11:07:55 +0530

    Create 18Feb25.cpp

---
+++ 2025/02 - FEB/18Feb25.cpp
@@ -0,0 +1,16 @@
+//      https://leetcode.com/problems/construct-smallest-number-from-di-string/description/
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        int s[10] = {'1'}, top = 0, j = 0;
+        for (int i = 0; i < pattern.length(); i++){
+            if (pattern[i] == 'I')
+                while (top >= 0) pattern[j++] = s[top--];
+            s[++top] = i + 1 + '1';
+        }
+        while (top > 0)
+            pattern[j++] = s[top--];
+        if (top == 0) pattern.push_back(s[top]);
+        return pattern;
+    }
+};
