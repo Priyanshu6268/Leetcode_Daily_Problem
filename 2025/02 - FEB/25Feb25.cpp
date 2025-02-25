a987fb8805a9cc3c4cd040f9fa5409593100b955
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-02-25 06:59:14 +0530

    Create 25Feb25.cpp

---
+++ 2025/02 - FEB/25Feb25.cpp
@@ -0,0 +1,14 @@
+//    https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/description/
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int res = 0, odd = 0, even = 0;
+        for (auto x: arr) {
+            even += 1;
+            if (x % 2)
+                swap(odd, even);
+            res = (res + odd) % 1000000007;
+        }
+        return res;
+    }
+};
