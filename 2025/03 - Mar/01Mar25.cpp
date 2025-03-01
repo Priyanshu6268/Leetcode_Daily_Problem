63c3d242727d584b10028e496dc5192be00c0828
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-03-01 09:41:01 +0530

    Create 01Mar25.cpp

---
+++ 2025/03 - Mar/01Mar25.cpp
@@ -0,0 +1,18 @@
+//    https://leetcode.com/problems/apply-operations-to-an-array/description/
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        const int n=nums.size();
+        vector<int> ans(n, 0);
+        int j=0;
+        for(int i=0; i<n-1; i++){
+            if (nums[i]==nums[i+1]){
+                nums[i]<<=1;
+                nums[i+1]=0;
+            }
+            if (nums[i]) ans[j++]=nums[i];
+        }
+        if (nums.back()) ans[j++]=nums.back();
+        return ans;
+    }
+};
