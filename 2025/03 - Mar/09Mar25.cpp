298a17accc1c5f2077bd9ba475c1a3f7e0efb048
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-03-09 06:48:58 +0530

    Create 09Mar25.cpp

---
+++ 2025/03 - Mar/09Mar25.cpp
@@ -0,0 +1,12 @@
+//    https://leetcode.com/problems/alternating-groups-ii/
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+    int ans = 0, count = 1, n = colors.size(); 
+    for(int i = 1; i < n + k-1; ++i){
+        count = ( colors[i % n] != colors[(n + i - 1)%n] )? count + 1: 1;
+        if(count >= k) ans++;
+    }
+    return ans;
+    }
+};
