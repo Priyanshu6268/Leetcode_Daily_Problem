2ecbdecaffa31e2b3bd2480d781a5bb6eb7a475e
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-03-11 09:13:45 +0530

    Create 11Mar25.cpp

---
+++ 2025/03 - Mar/11Mar25.cpp
@@ -0,0 +1,18 @@
+//    https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        unordered_map<char, int> freq;
+        int left = 0, cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i]]++;
+            while (freq.size() == 3) {
+                cnt += s.size() - i;
+                freq[s[left]]--;
+                if (freq[s[left]] == 0) freq.erase(s[left]);
+                left++;
+            }
+        }
+        return cnt;
+    }
+};
