2a9ca6b507e3b2c70496da119937ebd7cdfdc591
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-04-02 09:24:20 +0530

    Create 02Apr25.cpp

---
+++ 2025/04 - Apr/02Apr25.cpp
@@ -0,0 +1,14 @@
+//      https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/description/
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        const int n=nums.size();
+        long long L=nums[0], maxD=L-nums[1], ans=max(0LL, maxD*nums[2]);
+        for(int k=3; k<n; k++){
+            L=max(L, (long long)nums[k-2]);
+            maxD=max(maxD, L-nums[k-1]);
+            ans=max(ans, maxD*nums[k]);
+        }
+        return ans;
+    }
+};
