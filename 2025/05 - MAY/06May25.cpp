d9f0c20c61ef98dd9e0a79b627c24118b5194ed1
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-05-06 07:20:33 +0530

    Create 06May25.cpp

---
+++ 2025/05 - MAY/06May25.cpp
@@ -0,0 +1,11 @@
+//    https://leetcode.com/problems/build-array-from-permutation/
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans;
+        for(auto it : nums) {
+            ans.push_back(nums[it]);
+        }
+        return ans;
+    }
+};
