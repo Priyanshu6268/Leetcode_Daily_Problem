18c346b762f3dafbda0fdc812e6a509910e873a7
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-05-17 21:19:27 +0530

    Create 17May25.cpp

---
+++ 2025/05 - MAY/17May25.cpp
@@ -0,0 +1,16 @@
+//  https://leetcode.com/problems/sort-colors/description/
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero=0,one=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==0)zero++;
+        else if(nums[i]==1)one++;
+    }    
+    for(int i=0;i<nums.size();i++){
+        if(i<(zero))nums[i]=0;
+        else if(i<(one+zero))nums[i]=1;
+        else nums[i]=2;
+    }  
+    }
+};
