e148155a76da381f482504fbc7796260026d404f
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-07-01 09:11:55 +0530

    Create 01Jul25.cpp

---
+++ 2025/07 - July/01Jul25.cpp
@@ -0,0 +1,14 @@
+//    https://leetcode.com/problems/find-the-original-typed-string-i/description/
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n = word.length();
+        int count = n;
+        for (int i = 1; i < n; i++) {
+            if (word[i] != word[i - 1]) {
+                count--;
+            }
+        }
+        return count;
+    }
+};
