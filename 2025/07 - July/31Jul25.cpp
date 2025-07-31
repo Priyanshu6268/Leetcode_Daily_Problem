7ea16cfdf4e53f05aa03fb9767398cfe4b189d6b
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-07-31 13:08:22 +0530

    Create 31Jul25.cpp

---
+++ 2025/07 - July/31Jul25.cpp
@@ -0,0 +1,24 @@
+//    https://leetcode.com/problems/bitwise-ors-of-subarrays/description/
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& arr) {
+        unordered_set<int> res;
+        set<int> cur;
+
+        for (int num : arr) {
+            set<int> next;
+            next.insert(num);
+
+            for (int prev : cur) {
+                next.insert(prev | num);
+            }
+
+            cur = next;
+
+            for (int x : cur) {
+                res.insert(x);
+            }
+        }
+        return res.size();
+    }
+};
