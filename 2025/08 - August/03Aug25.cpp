9655c996998db41a43cd65cf4ad1c95d55bf2496
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-08-03 05:55:30 +0530

    Create 03Aug25.cpp

---
+++ 2025/08 - August/03Aug25.cpp
@@ -0,0 +1,38 @@
+//    https://leetcode.com/problems/maximum-fruits-harvested-after-at-most-k-steps/description/
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int total = 0;
+        int size = std::max(startPos, fruits.back().front()) + 1;
+        std::vector<int> prefix_sum(size, 0);
+
+        for (auto& it : fruits) prefix_sum[it[0]] = it[1];
+        for (int i = 1; i < size; ++i) prefix_sum[i] += prefix_sum[i - 1];
+
+        int lsteps, rsteps;
+        for (int r = startPos; r < size && r <= startPos + k; ++r)
+        {
+            rsteps = r - startPos;
+            lsteps = k - 2 * rsteps;
+            if (lsteps < 0)
+                lsteps = 0;
+            int l = startPos - lsteps;
+            if (l < 0)
+                l = 0;
+            total = std::max(total, prefix_sum[r] - (l > 0 ? prefix_sum[l - 1] : 0));
+        }
+
+        for (int l = startPos; l >= 0 && l >= startPos - k; --l)
+        {
+            lsteps = startPos - l;
+            rsteps = k - 2 * lsteps;
+            if (rsteps < 0)
+                rsteps = 0;
+            int r = startPos + rsteps;
+            if (r >= size)
+                r = size - 1;
+            total = std::max(total, prefix_sum[r] - (l > 0 ? prefix_sum[l - 1] : 0));
+        }
+        return total;
+    }
+};
