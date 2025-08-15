7adbfb4d397223ea97c58fc17399e2c2f8df5aa8
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-08-15 10:39:18 +0530

    Create 15Aug25.cpp

---
+++ 2025/08 - August/15Aug25.cpp
@@ -0,0 +1,7 @@
+//    https://leetcode.com/problems/power-of-four/description/
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
+    }
+};
