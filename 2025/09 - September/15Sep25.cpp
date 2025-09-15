1a7bacbd8675f6703cbc0cbfd632891238665c01
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-09-15 10:22:41 +0530

    Create 15Sep25.cpp

---
+++ 2025/09 - September/15Sep25.cpp
@@ -0,0 +1,24 @@
+//    https://leetcode.com/problems/maximum-number-of-words-you-can-type/
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        stringstream ss(text);
+        string word;
+        int count = 0;
+
+        while (ss >> word) {
+            bool canBeTyped = true;
+            for (char ch : word) {
+                if (brokenLetters.find(ch) != string::npos) {
+                    canBeTyped = false;
+                    break;
+                }
+            }
+
+            if (canBeTyped) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
