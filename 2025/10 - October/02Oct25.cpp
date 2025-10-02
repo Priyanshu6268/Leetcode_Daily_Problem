5bc6e95fda0542362aa800d78d4e4bbc89da15e2
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-10-02 20:20:29 +0530

    Create 02Oct25.cpp

---
+++ 2025/10 - October/02Oct25.cpp
@@ -0,0 +1,11 @@
+//    https://leetcode.com/problems/water-bottles-ii/
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int x) {
+        int ans = numBottles;
+        while (numBottles >= x) {
+            numBottles -= x - 1, x++, ans++;
+        }
+        return ans;
+    }
+};
