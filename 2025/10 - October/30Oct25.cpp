965df16cbc7e6af40d3a70d04c55f4c8b70c6592
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-10-30 05:57:59 +0530

    Create 30Oct25.cpp

---
+++ 2025/10 - October/30Oct25.cpp
@@ -0,0 +1,11 @@
+//    https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/description/
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int ans = target[0];
+        for (int i = 1; i < target.size(); i++) {
+            ans += max(0, target[i] - target[i - 1]);
+        }
+        return ans;
+    }
+};
