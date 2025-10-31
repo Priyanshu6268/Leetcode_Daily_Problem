1eb318e890840eca340f62040e21202bdc9adcf8
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-10-31 08:51:11 +0530

    Create 31Oct25.cpp

---
+++ 2025/10 - October/31Oct25.cpp
@@ -0,0 +1,17 @@
+//    https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/description/
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        bitset<101> b;
+        int k = 0, n = nums.size();
+        for (int i = 0; i != n && k != 2; ++i) {
+            int v = nums[i];
+            nums[k] = v;
+            k += b[v];
+            b[v] = 1;
+        }
+
+        nums.resize(2);
+        return std::move(nums);
+    }
+};
