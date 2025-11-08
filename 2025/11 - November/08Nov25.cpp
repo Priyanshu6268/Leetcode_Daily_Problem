0321d4f1e2c4332c08503fbef9193d5482959724
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-11-08 14:02:51 +0530

    Create 08Nov25.cpp

---
+++ 2025/11 - November/08Nov25.cpp
@@ -0,0 +1,10 @@
+//    https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if (n==0) return 0;
+        int k=31-__builtin_clz(n);
+        int numOp=(1<<(k+1))-1;
+        return numOp-minimumOneBitOperations(n^(1<<k));
+    }
+};
