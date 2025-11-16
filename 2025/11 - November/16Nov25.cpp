a7927c28c6abff05e90d197895aa2b410a518331
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-11-16 06:13:07 +0530

    Create 16Nov25.cpp

---
+++ 2025/11 - November/16Nov25.cpp
@@ -0,0 +1,21 @@
+//    https://leetcode.com/problems/number-of-substrings-with-only-1s/description/
+class Solution {
+public:
+    int numSub(string s) {
+        int n = s.size();
+        int ans = 0;
+        const int mod = 1e9 + 7;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                cnt = (cnt + 1) % mod;
+            } else {
+                cnt = 0;
+            }
+
+            ans = (ans + cnt) % mod;
+        }
+        return ans;
+    }
+};
