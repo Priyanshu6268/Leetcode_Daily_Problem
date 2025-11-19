12069f0ed7e61a5e1c5b8ae6f9a0643763d7f04c
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-11-19 12:31:33 +0530

    Create 19Nov25.cpp

---
+++ 2025/11 - November/19Nov25.cpp
@@ -0,0 +1,14 @@
+//    https://leetcode.com/problems/keep-multiplying-found-values-by-two/description/
+class Solution {
+public:
+    static int findFinalValue(const vector<int>& a, const int k) {
+        unsigned int bits = 0;
+        for (const int x : a) {
+            const auto [q, r] = div(x, k);
+            if (r == 0 && popcount((unsigned)q) == 1) {
+                bits |= q;
+            }
+        }
+        return k * (1 << countr_one(bits));
+    }
+};
