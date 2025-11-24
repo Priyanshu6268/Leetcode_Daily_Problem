1ce3668dd8607339f81cae9d56d94b3a9c1b2497
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-11-24 10:11:12 +0530

    Create 24Nov25.cpp

---
+++ 2025/11 - November/24Nov25.cpp
@@ -0,0 +1,14 @@
+//         https://leetcode.com/problems/binary-prefix-divisible-by-5/description/
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> ans;
+        int x = 0;
+
+        for(int bit : nums) {
+            x = (x * 2 + bit) % 5;
+            ans.push_back(x == 0);
+        }
+        return ans;
+    }
+};
