0083c9bfac762b0e9e6d199f77fad3bbfc4e75a8
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-11-29 07:15:38 +0530

    Create 29Nov25.cpp

---
+++ 2025/11 - November/29Nov25.cpp
@@ -0,0 +1,7 @@
+//    http://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k/description/
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        return accumulate(nums.begin(), nums.end(), 0)%k;
+    }
+};
