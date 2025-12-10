ace1427339105a1ad16f67287332090aba8175e2
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-12-10 10:27:00 +0530

    Create 10Dec25.cpp

---
+++ 2025/12 - December/10Dec25.cpp
@@ -0,0 +1,18 @@
+//    https://leetcode.com/problems/count-the-number-of-computer-unlocking-permutations/description/
+class Solution {
+public:
+    static int countPermutations(vector<int>& complexity) {
+        const int x0=complexity[0], 
+        x1=*min_element(complexity.begin()+1, complexity.end()), 
+        mod=1e9+7, n=complexity.size();
+
+        if (x0>=x1) return 0;
+        long long cnt=1;
+        for(int i=1; i<=n-1; i++){
+            cnt*=i;
+            if (cnt>=mod) cnt%=mod;
+        }
+        return cnt;
+        
+    }
+};
