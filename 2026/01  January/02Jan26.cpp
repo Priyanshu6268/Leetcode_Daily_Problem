3d9e1b6559f4d6a67173e5f28269d31a4216bc2c
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2026-01-02 09:33:58 +0530

    Create 02Jan26.cpp

---
+++ 2026/01  January/02Jan26.cpp
@@ -0,0 +1,12 @@
+//    https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        bitset<10001> seen=0;
+        for(int x: nums){
+            if (seen[x]) return x;
+            seen[x]=1;
+        }
+        return -1;
+    }
+};
